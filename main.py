592863943854efb184f2971c91f2291d4cfec949
Theo F. <168644391+theodorismmmm@users.noreply.github.com>
2026-08-14 13:03:55 +0200

    code
    Added

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import time
+import os
+import sys
 
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def main():
+    clear_screen()
+    
+    print("Swift Correction & Results Engine")
+    
+    time.sleep(3)
+    
+    clear_screen()
+    image_path = input("Please paste the file path of the image: ")
+    
+    sys.exit()
+
+if __name__ == "__main__":
+    main()
